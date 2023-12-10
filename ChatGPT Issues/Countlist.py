71c448469da1bad060cdfeb2d5fc5224fd27419a
nakazi <nakazi1209@gmail.com>
2023-12-10 17:55:10 +0900

    add answer

---
+++ ChatGPT Issues/Countlist.py
@@ -0,0 +1,24 @@
+# 問題:
+# あなたは与えられたリスト内の各要素の出現回数を数えるPython関数を書く必要があります。この関数は、リスト内の各要素とその出現回数を辞書形式で返すべきです。
+
+# 具体的なタスク:
+# 関数 count_duplicates を定義してください。
+# この関数は、任意の要素（数値、文字列など）を含むリストを引数として受け取ります。
+# 関数は、リスト内の各要素の出現回数をカウントし、要素をキーとし、その出現回数を値とする辞書を返す必要があります。
+
+# 例:
+# 入力: ['apple', 'banana', 'apple', 'orange', 'banana', 'apple']
+# 出力: {'apple': 3, 'banana': 2, 'orange': 1}
+
+def count_duplicates(items):
+    counts = {}
+    for i in items:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    return counts
+
+if __name__ == '__main__':
+    result = count_duplicates(['apple', 'banana', 'apple', 'orange', 'banana', 'apple'])
+    print(result)
